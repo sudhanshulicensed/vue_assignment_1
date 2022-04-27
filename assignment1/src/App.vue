4d061dec283363930ab3de9b10193a58ce0b7c62
sudhanshulicensed <sudhanshu.licensed@gmail.com>
2022-04-27 18:09:45 +0530

    Added Basic files

--- assignment1/src/App.vue
+++ assignment1/src/App.vue
@@ -1,28 +1,58 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>Customer Details</p>
+    <div class="inputs">
+      <form v-on:submit.prevent>
+        <label for="fname">First Name</label>
+        <input v-model="finame" id="fname" type="text" placeholder="Enter your First Name">
+        <label for="lname">Last Name</label>
+        <input v-model="liname" id="lname" type="text" placeholder="Enter your Last Name">
+        <label for="p-address">Address</label>
+        <input v-model="address" id="p-address" type="text" placeholder="Enter your Address">
+      </form>
+      <button>Add</button>
+    </div>
+    <div class="tables">
+      <table>
+        <tr>
+        <td>{{ finame }}</td>
+        <td>{{ liname }}</td>
+        <td>{{ address }}</td>
+      </tr>
+      </table>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return{
+      finame: "Sky",
+      liname: "High",
+      address: "Clouds",
+    }
+
+  },
+  methods: {
+
   }
 }
 </script>
 
 <style>
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.inputs{
+  width: 100%;
 }
 </style>
